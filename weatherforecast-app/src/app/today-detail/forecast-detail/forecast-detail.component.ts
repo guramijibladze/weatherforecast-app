38055581f1856d7cb6a2a7cc3d91a5eb821dea19
guramijibladze <guramijibladze@gmail.com>
2021-08-03 20:26:14 +0400

    added

--- weatherforecast-app/src/app/today-detail/forecast-detail/forecast-detail.component.ts
+++ weatherforecast-app/src/app/today-detail/forecast-detail/forecast-detail.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forecast-detail.component.scss']
 })
 export class ForecastDetailComponent implements OnInit {
+  hours
+  minutes
+  color:boolean
 
+  getCurrentDate(){
+    setInterval(() => {
+      this.hours = new Date().getHours();
+      this.minutes = new Date().getMinutes();
+    }, 1000)
+
+    if(this.hours > 18){
+     this.color = false
+    }
+  }
+
+
+  // get(){
+  //   this.hours = new Date().getHours();
+  //   this.minutes = new Date().getMinutes() 
+  // }
   constructor() { }
 
   ngOnInit(): void {
+    this.getCurrentDate()
+    
   }
 
 }
